1787bb7474043aa3884265e137d2163639a9dcd2
unknown <harilalrafaliya@gmail.com>
2020-03-11 15:53:30 +0530

    make it better

---
+++ build/precache-manifest.c8c4a3a6b0a3f773ecbd07bb98e0fca7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "31f0fcfaa2086600b01a11aa59f40e77",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c20ef8a9e2410c422646",
+    "url": "/static/css/main.8f79d1be.chunk.css"
+  },
+  {
+    "revision": "a3cce27ad4bf158d802c",
+    "url": "/static/js/2.9f8567bd.chunk.js"
+  },
+  {
+    "revision": "c20ef8a9e2410c422646",
+    "url": "/static/js/main.04acc7c7.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
