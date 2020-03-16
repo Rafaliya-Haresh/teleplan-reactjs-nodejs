35a34edc5c5a82f1edc7d7455c6cd606b93e7822
unknown <harilalrafaliya@gmail.com>
2020-03-16 16:57:37 +0530

    fixed

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ var APPLICATION_URL = 'https://tlpt2.moh.hnet.bc.ca';
 // File upload
 var storage = multer.diskStorage({
     destination: function (req, file, cb) {
-      cb(null, './uploads/log')
+      cb(null, './public/log')
     },
     filename: function (req, file, cb) {
       cb(null, Date.now() + file.originalname )
@@ -23,7 +23,7 @@ var uploadFile = multer({ storage: storage }).single('submitFile');
 // Ascii upload
 var storageAscii = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, './uploads/ascii')
+    cb(null, './public/ascii')
   },
   filename: function (req, file, cb) {
     cb(null, Date.now() + file.originalname )
